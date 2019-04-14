1465c9ffa98d7b01add685e56f4ce63d53835355
Игорь Колонтарь <idk@MacBook-Pro-Igor.local>
2019-04-14 22:47:35 +0300

    +initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Menubar from './Menubar';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Menubar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Menubar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function Menubar ()
+ {
+  render() {
+            return (
+              <div className = "Menubar">Dis is menubar!</div>
+          );
+            }
+}
+
+export default Menubar;
